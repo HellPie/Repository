467bb2b3213c5ab9d6477799b676d55829e174fa
HellPie <git@hellpie.dev>
2021-11-23 17:08:54 +0200

    Implement overflow and underflow checks in atoi()

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,16 +6,17 @@
 /*   By: drossi <drossi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 19:43:04 by drossi            #+#    #+#             */
-/*   Updated: 2021/11/08 14:20:43 by drossi           ###   ########.fr       */
+/*   Updated: 2021/11/23 16:14:04 by drossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_defs.h"
+#include "ft_limits.h"
 #include "ft_ctype.h"
 
 int	ft_atoi(const char *s)
 {
-	int		number;
+	long	number;
 	t_bool	is_negative;
 
 	number = 0;
@@ -25,8 +26,14 @@ int	ft_atoi(const char *s)
 	if (*s == '-' || *s == '+')
 		is_negative = *s++ == '-';
 	while (ft_isdigit(*s))
+	{
 		number = 10 * number - (*s++ - '0');
+		if (number > FT_INT_MAX && !is_negative)
+			return (FT_INT_MAX);
+		else if (number < FT_INT_MIN && is_negative)
+			return (FT_INT_MIN);
+	}
 	if (is_negative)
-		return (number);
-	return (-number);
+		return ((int)number);
+	return ((int) -number);
 }
