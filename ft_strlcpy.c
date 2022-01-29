5c05c9b27825c34cfa56436f5921b5bc5b008985
HellPie <git@hellpie.dev>
2022-01-29 18:10:30 +0200

    Protect more methods

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: drossi <drossi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 15:04:33 by drossi            #+#    #+#             */
-/*   Updated: 2022/01/29 01:19:14 by drossi           ###   ########.fr       */
+/*   Updated: 2022/01/29 18:02:23 by drossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ size_t	ft_strlcpy(char *d, const char *s, size_t n)
 {
 	char	*dest;
 
-	if (!n--)
+	if (s == NULL)
+		return (0);
+	if (d == NULL || !n--)
 		return (ft_strlen(s));
 	dest = d;
 	while (n)
