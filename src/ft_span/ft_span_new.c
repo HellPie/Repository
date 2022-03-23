79bb6990f93cb156935cc6deb67f416bebd3d6fd
HellPie <git@hellpie.dev>
2022-03-23 14:38:58 +0200

    Fix span_new reliance on signed values when size_t is unsigned

--- src/ft_span/ft_span_new.c
+++ src/ft_span/ft_span_new.c
@@ -6,7 +6,7 @@
 /*   By: drossi <drossi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 15:31:09 by drossi            #+#    #+#             */
-/*   Updated: 2022/03/22 17:57:01 by drossi           ###   ########.fr       */
+/*   Updated: 2022/03/23 14:38:27 by drossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 
 size_t	ft_span_new(t_span *out, size_t c_item, size_t s_item)
 {
-	if (!out || c_item <= 0 || s_item <= 0)
-		return (-1);
+	if (!out || c_item == 0 || s_item == 0)
+		return (0);
 	out->s_item = s_item;
 	out->s_data = c_item * s_item;
 	out->len = 0;
 	out->data = malloc(out->s_data);
 	if (!out->data)
-	{
 		ft_span_del(out);
-		return (-1);
-	}
 	return (out->s_data);
 }
