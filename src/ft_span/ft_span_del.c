bdf10e4d5eb804bcddcbc00de0d6b0a53f6b1f36
HellPie <git@hellpie.dev>
2022-03-23 14:41:50 +0200

    Refactor span_del to be considerably simpler

--- src/ft_span/ft_span_del.c
+++ src/ft_span/ft_span_del.c
@@ -6,21 +6,20 @@
 /*   By: drossi <drossi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 17:40:54 by drossi            #+#    #+#             */
-/*   Updated: 2022/03/22 17:50:59 by drossi           ###   ########.fr       */
+/*   Updated: 2022/03/23 14:41:02 by drossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "ft_extra.h"
 #include "ft_span.h"
 
 void	ft_span_del(t_span *span)
 {
-	if (!span)
-		return ;
-	if (span->data)
-		free(span->data);
-	span->data = NULL;
-	span->s_data = 0;
-	span->s_item = 0;
-	span->len = 0;
+	if (span)
+	{
+		ft_memdel(&span->data);
+		span->s_data = 0;
+		span->s_item = 0;
+		span->len = 0;
+	}
 }
