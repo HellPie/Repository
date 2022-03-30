1b51571940d039e10e63c107620e6e8e0e4028bf
HellPie <git@hellpie.dev>
2022-03-30 16:22:39 +0300

    Remove errno.h

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: drossi <drossi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 18:31:57 by drossi            #+#    #+#             */
-/*   Updated: 2021/11/19 22:13:33 by drossi           ###   ########.fr       */
+/*   Updated: 2022/03/30 16:15:36 by drossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define LIBFT_H
 
 # include "ft_limits.h"
-# include "ft_errno.h"
 
 # include "ft_ctype.h"
 # include "ft_stdlib.h"
